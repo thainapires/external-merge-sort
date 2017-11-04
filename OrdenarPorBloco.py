4a42f84393ace4266c00ba8bbc8b7c65a42d5aa0
thainaspires <thainaspiress2@gmail.com>
2017-11-04 02:50:40 -0200

    Ordenacao em blocos e Intercalacao
    Programas com a ordenacao por blocos e a intercalacao

--- OrdenarPorBloco.py
+++ OrdenarPorBloco.py
@@ -1,22 +1,24 @@
 import struct
 import sys
 import os
+import time
 
-#Thaina Simoes Pires
-#Organizacao e estrutura de arquivos
-#Arquivo para odenacaoo de blocos com o numero de linhas escolido pelo usuario
-#Python 2.7.14
+#ARQUIVO DE ORDENACAO DE BLOCOS COM TAMANHOS ESCOLHIDOS PELO USUARIO
 
 if len(sys.argv) != 2:
 	tamBloco = int(input("Entre com o numero de linhas de cada bloco: "))
 else:
 	tamBloco = int(sys.argv[1])
 
+inicio = time.time()
+
 registroCEP = struct.Struct("72s72s72s72s2s8s2s")
 cepColumn = 5
-print "Tamanho da Estrutura: %d" % registroCEP.size
+print "Tamanho de cada linha do arquivo: %d" % registroCEP.size
+
 f = open("cep.dat","rb+")
 fAux = open("cepBlocos.dat","wb+")
+
 tam = os.path.getsize("cep.dat")/registroCEP.size
 tamBlocos = []
 qtdBlocos = tam / tamBloco
@@ -25,6 +27,7 @@ tamBlocos = [tamBloco] * qtdBlocos
 if resto > 0:
 	tamBlocos.append(resto)
 
+#Funcao para comparacao de tuplas
 def cmp ( ta, tb ):
 	if ta[cepColumn] == tb[cepColumn]: return 0
 	if ta[cepColumn] > tb[cepColumn]: return 1
@@ -45,6 +48,11 @@ for i in tamBlocos:
 		line_pack = registroCEP.pack(listOrdena[j][0], listOrdena[j][1], listOrdena[j][2], listOrdena[j][3], listOrdena[j][4], listOrdena[j][5], listOrdena[j][6])
 		fAux.write(line_pack) #escreve no mesmo arquivo
 	listOrdena = []
+	
 print "Fim do programa"
+
 f.close()
 fAux.close()
+
+final = time.time() - inicio
+print "Tempo: ",final
